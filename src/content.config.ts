524896a14d554c5f712e7e72c018ec38c94cbdea
ksquaredcoding <ksquaredcoding@gmail.com>
2025-11-05 18:12:19 -0700

    init kf v1 website

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({
+    loader: glob({ pattern: "**/*.json", base: "./src/projects" }),
+    schema: z.object({
+        title: z.string(),
+        blurb: z.string(),
+        client: z.string(),
+        images: z.array(z.string()),
+        year: z.array(z.number()).optional(),
+    }),
+ });
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { projects };
